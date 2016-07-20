22116d170a520651c93172ee982a5a51a2678448
temi <temivarg@gmail.com>
2016-07-20 17:24:35 +1000

    added groupId removed commented lines

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.groupId = "au.org.ala"
 
 grails.project.fork = [
     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
