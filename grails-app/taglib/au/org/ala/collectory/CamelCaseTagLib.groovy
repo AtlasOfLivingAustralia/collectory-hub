83141c80fb3d87853b5ec4603274e995805bf1ad
Javier Molina <Javier.Molina@csiro.au>
2016-10-07 16:37:27 +1100

    #sandbox-38 - Reenable charts config function TODO - Return to the right view if coming from adminList

---
+++ grails-app/taglib/au/org/ala/collectory/CamelCaseTagLib.groovy
@@ -0,0 +1,18 @@
+package au.org.ala.collectory
+
+class CamelCaseTagLib {
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+
+    def formatService
+
+    def formatFieldName = { attr, body ->
+        def fieldName = attr['value']
+        out << formatService.formatFieldName(fieldName)
+    }
+
+    def prettyCamel = { attr, body ->
+        def value = attr['value]']
+        out << formatService.prettyCamel(value)
+    }
+}
